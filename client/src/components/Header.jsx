3e15f2e57197c86dd497e604f7f945717411d273
thanakitpw <thanakitpw@gmail.com>
2024-10-30 23:46:29 +0700

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,9 @@ const Header = () => {
                <FaMoon />
             </Button>
             <Link to="/sign-in">
-               <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+               <Button gradientDuoTone="purpleToBlue" outline>
+                  Sign In
+               </Button>
             </Link>
             <NavbarToggle />
          </div>
