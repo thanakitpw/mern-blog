940943e3b9008aa901a3542eeefebf8646f85804
thanakitpw <thanakitpw@gmail.com>
2024-10-31 15:13:37 +0700

    add functionality to the sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,44 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+   const [formData, setFormData] = useState({});
+   const [errorMessage, setErrorMessage] = useState(null);
+   const [loading, setLoading] = useState(false);
+   const navigate = useNavigate();
+
+   const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      if (!formData.username || !formData.email || !formData.password) {
+         return setErrorMessage("Please fill out all field.");
+      }
+      try {
+         setLoading(true);
+         setErrorMessage(null);
+         const res = await fetch("/api/auth/signup", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData),
+         });
+         const data = await res.json();
+         if (data.success === false) {
+            return setErrorMessage(data.message);
+         }
+         if (res.ok) {
+            navigate("/sign-in");
+         }
+         setLoading(false);
+      } catch (error) {
+         setErrorMessage(error.message);
+         setLoading(false);
+      }
+   };
+
    return (
       <div className="min-h-screen mt-20">
          <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -20,33 +57,47 @@ const SignUp = () => {
             </div>
             {/* Right */}
             <div className="flex-1">
-               <form className="flex flex-col gap-4">
+               <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                   <div className="">
                      <Label value="Username" />
                      <TextInput
                         type="text"
                         placeholder="Username"
                         id="username"
+                        onChange={handleChange}
                      />
                   </div>
                   <div className="">
                      <Label value="Email" />
                      <TextInput
-                        type="text"
+                        type="email"
                         placeholder="name@company.com"
                         id="email"
+                        onChange={handleChange}
                      />
                   </div>
                   <div className="">
                      <Label value="Password" />
                      <TextInput
-                        type="text"
+                        type="password"
                         placeholder="Password"
                         id="password"
+                        onChange={handleChange}
                      />
                   </div>
-                  <Button gradientDuoTone="purpleToPink" type="submit">
-                     Sign Up
+                  <Button
+                     gradientDuoTone="purpleToPink"
+                     type="submit"
+                     disabled={loading}
+                  >
+                     {loading ? (
+                        <>
+                           <Spinner size="sm" />
+                           <span className="pl-3">Loading...</span>
+                        </>
+                     ) : (
+                        "Sign Up"
+                     )}
                   </Button>
                </form>
                <div className="flex gap-2 text-sm mt-5">
@@ -55,6 +106,11 @@ const SignUp = () => {
                      Sign In
                   </Link>
                </div>
+               {errorMessage && (
+                  <Alert className="mt-5" color="failure">
+                     {errorMessage}
+                  </Alert>
+               )}
             </div>
          </div>
       </div>
